5f4727af84b0370970b1f463157218f6d7468eef
hyewon <cherryJubilee@naver.com>
2023-05-22 21:58:48 +0900

    feat: [비밀번호 찾기] - 비밀번호 찾기 backend 부분 구현 - phw

--- THEBeautySpring/src/main/java/com/kosa/theBeauty/user/domain/UserVO.java
+++ THEBeautySpring/src/main/java/com/kosa/theBeauty/user/domain/UserVO.java
@@ -1,5 +1,17 @@
 package com.kosa.theBeauty.user.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserVO {
+	
+    private String userName;
+    private String userMail;
+    private String userPw;
+	private int userRegistration;
 
 }
--- THEBeautySpring/src/main/java/com/kosa/theBeauty/user/service/UserService.java
+++ THEBeautySpring/src/main/java/com/kosa/theBeauty/user/service/UserService.java
@@ -1,5 +1,21 @@
 package com.kosa.theBeauty.user.service;
 
+import org.springframework.stereotype.Service;
+
+import com.kosa.theBeauty.user.dao.UserDAO;
+import com.kosa.theBeauty.user.domain.UserVO;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+	
+	private final UserDAO dao;
+
+    public UserVO findPassword(String userMail, int userRegistration) {
+        
+    	return dao.findPassword(userMail, userRegistration);
+    }
 
 }
