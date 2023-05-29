3930d0baed7d58d954a1f510090248fdac470a45
kosaComputerKww <dyffh1031@gmail.com>
2023-05-29 20:42:25 +0900

    feat: [화상채팅] 컨설턴트 본인 브랜드 품목 불러오기 - kww

--- THEBeautySpring/src/main/java/com/kosa/theBeauty/product/dao/ProductDAO.java
+++ THEBeautySpring/src/main/java/com/kosa/theBeauty/product/dao/ProductDAO.java
@@ -1,12 +1,16 @@
 package com.kosa.theBeauty.product.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.kosa.theBeauty.product.domain.ProductDetailVO;
+import com.kosa.theBeauty.product.domain.ProductVO;
 
 @Mapper
 public interface ProductDAO {
 	
 	ProductDetailVO getProductDetails(int product_seq);
-
+	
+	List<ProductVO> selectProductByBrandName(String brandName);
 }
