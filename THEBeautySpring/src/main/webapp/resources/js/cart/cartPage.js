1cbdfe9502e6c21b87db40da42a0ba6d6761fa51
cherryJubilee <hyewon5026@gmail.com>
2023-06-01 23:40:07 +0900

    feat: [장바구니]  장바구니 상품 개수 증감 frontend 구현 중 -  phw

--- THEBeautySpring/src/main/webapp/resources/js/cart/cartPage.js
+++ THEBeautySpring/src/main/webapp/resources/js/cart/cartPage.js
@@ -17,3 +17,51 @@ $(document).ready(function() {
   // 상품삭제 버튼 클릭 이벤트 처리
   $(".btn.color15").on("click", deleteProduct);
 });
+
+
+
+
+// 장바구니 상품 개수 조절
+function productCounting(operation, productSeq) {
+    let currentCount = parseInt($('#productCount-'+productSeq).val());
+
+    // count up and down operation
+    if (operation === '+') {
+        currentCount += 1;
+    } else if (operation === '-') {
+        if (currentCount > 0) {
+            currentCount -= 1;
+        } else {
+            alert("Product count can't be less than 0");
+            return;
+        }
+    }
+
+    // update product count in the input field
+    $('#productCount-'+productSeq).val(currentCount);
+
+    // Here goes your Ajax request for updating product count in the server
+    $.ajax({
+        type: 'POST',
+        url: '/theBeauty/cart/updateProductCount', // use the url for updating product count in your backend
+        data: {
+            productSeq: productSeq,
+            productCount: currentCount
+        },
+        success: function(response) {
+            // Update price on the page based on the new count
+            updatePrice(response, productSeq);
+        },
+        error: function(error) {
+            console.log('Error in updating product count', error);
+        }
+    });
+}
+
+function updatePrice(productDetail, productSeq) {
+    // Assuming productDetail has price and count
+    let newPrice = productDetail.productPrice * productDetail.productCount;
+    
+    // Update price on the page
+    $('#item-price-'+productSeq).text(newPrice);
+}
