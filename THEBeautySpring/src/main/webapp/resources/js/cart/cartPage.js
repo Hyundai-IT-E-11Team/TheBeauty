3b25f35eeeef8f3a0628e88b849785bb55d77e79
cherryJubilee <hyewon5026@gmail.com>
2023-06-04 17:31:38 +0900

    fix: [장바구니] 장바구니 로드시 상품 가격을 한국화폐 형식으로 수정 - phw

--- THEBeautySpring/src/main/webapp/resources/js/cart/cartPage.js
+++ THEBeautySpring/src/main/webapp/resources/js/cart/cartPage.js
@@ -1,84 +1,92 @@
 $(document).ready(function() {
 	
-  // 상품삭제 함수 정의
-  deleteProduct = function() {
-	let container = $(this);
-    var productSeq = $(this).data('product-id');
-    $.ajax({
-      url: '/theBeauty/cart/deleteProduct',
-      type: 'POST',
-      data: { productSeq: productSeq },
-      success: function(response) {
-        container.closest(".product").remove();
-        updateTotalPrice(); // 상품 삭제 할 때 총 합계 업데이트
-      }
-    });
-  }
+	// 한국 화폐 형식으로 변환하는 함수
+	function formatCurrency(num) {
+		return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+	}
 
-  // 상품삭제 버튼 클릭 이벤트 처리
-  $(".btn.color15").on("click", deleteProduct);
-  
-  // 장바구니 페이지가 로드되면 총 가격을 계산
-  updateTotalPrice();
-  
+
+	// 상품삭제 함수 정의
+	deleteProduct = function() {
+		let container = $(this);
+		var productSeq = $(this).data('product-id');
+		$.ajax({
+			url : '/theBeauty/cart/deleteProduct',
+			type : 'POST',
+			data : {
+				productSeq : productSeq
+			},
+			success : function(response) {
+				container.closest(".product").remove();
+				updateTotalPrice(); // 상품 삭제 할 때 총 합계 업데이트
+			}
+		});
+	}
+	
+
+	// 상품삭제 버튼 클릭 이벤트 처리
+	$(".btn.color15").on("click", deleteProduct);
+
+	
+	// 페이지 로딩 시 각 상품 가격을 한국 화폐 형식으로 표시
+	$(".item-price").each(function() {
+		let price = $(this).data("price");
+		$(this).text(formatCurrency(price) + " 원");
+	});
+	
+	
+	// 장바구니 페이지가 로드되면 총 가격을 계산
+	updateTotalPrice();
+
 });
 
 
 // 장바구니 상품 개수 조절
 function productCounting(operation, productSeq) {
-	let productCountElement  = document.getElementById("productCount-"+productSeq);
+	let productCountElement = document.getElementById("productCount-"
+			+ productSeq);
 	let currentCount = parseInt(productCountElement.value);
 
-    let productPriceElement = document.getElementById("itemPrice-"+productSeq);
-    let productPrice = parseFloat(productPriceElement.getAttribute("data-price"));
+	let productPriceElement = document
+			.getElementById("itemPrice-" + productSeq);
+	let productPrice = parseFloat(productPriceElement
+			.getAttribute("data-price"));
 
-    if (operation === '+') {
-        currentCount += 1;
-    } else if (operation === '-') {
-        if (currentCount > 1) {
-            currentCount -= 1;
-        } else {
-            alert("상품의 수량이 0개보다 많아야 합니다.");
-            return;
-        }
-    }
+	if (operation === '+') {
+		currentCount += 1;
+	} else if (operation === '-') {
+		if (currentCount > 1) {
+			currentCount -= 1;
+		} else {
+			alert("상품의 수량이 0개보다 많아야 합니다.");
+			return;
+		}
+	}
 
-    productCountElement.value = currentCount;
+	productCountElement.value = currentCount;
 
-    // 장바구니 개수에 따른 가격 변경
-    let totalPrice = currentCount * productPrice;
-    productPriceElement.innerHTML = totalPrice.toLocaleString("ko-KR") + "원";
-    updateTotalPrice()
+	
+	// 장바구니 개수에 따른 가격 변경
+	let totalPrice = currentCount * productPrice;
+	productPriceElement.innerHTML = totalPrice.toLocaleString("ko-KR") + "원";
+	updateTotalPrice()
 }
 
-//장바구니의 총 가격을 계산하고 해당 값을 페이지에 업데이트하는 함수
+
+// 장바구니의 총 가격을 계산하고 해당 값을 페이지에 업데이트하는 함수
 function updateTotalPrice() {
-    let total = 0;
+	let total = 0;
 
-    $(".item-price").each(function() {
-        let itemPrice = parseFloat($(this).attr("data-price"));
-        let itemCount = parseInt($(this).closest(".product").find(".onlyNumberInput").val());
-        total += itemPrice * itemCount;
-    });
-    
-    
-    total = total.toLocaleString("ko-KR");
+	$(".item-price").each(
+			function() {
+				let itemPrice = parseFloat($(this).attr("data-price"));
+				let itemCount = parseInt($(this).closest(".product").find(
+						".onlyNumberInput").val());
+				total += itemPrice * itemCount;
+			});
 
-    $("#totOrdPrc").text(total);
-    // 아래 코드를 추가하여 상품금액에도 같은 값을 설정합니다.
-    $("#totProdPrc").text(total + " 원");
+	total = total.toLocaleString("ko-KR");
+
+	$("#totOrdPrc").text(total);
+	$("#totProdPrc").text(total + " 원");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
